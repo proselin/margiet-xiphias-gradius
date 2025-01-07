e6941610cc00cfd721bf384ba9c893ab8f7120c6
Hung Nguyen Quoc <quochung9401@gmail.com>
2025-01-07 10:09:28 +0700

    Initialize

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import path from 'path';
 
 export default defineConfig({
   root: __dirname,
@@ -20,6 +21,11 @@ export default defineConfig({
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
   // },
+  resolve:{
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
   build: {
     outDir: './dist/margiet-xiphias-gradius',
     emptyOutDir: true,
