cce37a613dad2161f41a0e63197f2133ccb0b00b
Hung Nguyen Quoc <quochung9401@gmail.com>
2025-01-07 09:53:39 +0700

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="margiet-xiphias-gradius" />
+    </div>
+  );
+}
+
+export default App;
